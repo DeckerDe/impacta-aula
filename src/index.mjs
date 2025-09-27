13a9fb390c06754e1871b7e5a9f5baff13f014ef
Deiwid <deiwidecker@gmail.com>
2025-09-27 04:50:50 -0300

    feat: add css inline para servir o css

--- src/index.mjs
+++ src/index.mjs
@@ -1,6 +1,12 @@
 import fs from "fs";
 
-const html = fs.readFileSync("./index.html", "utf8");
+const htmlTemplate = fs.readFileSync("impacta.html", "utf8");
+const css = fs.readFileSync("styles.css", "utf8");
+
+const html = htmlTemplate.replace(
+  '<link rel="stylesheet" href="styles.css" />',
+  `<style>${css}</style>`
+);
 
 const handler = async (event, context, callback) => {
   const response = {
